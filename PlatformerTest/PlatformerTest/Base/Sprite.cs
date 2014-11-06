30f0f8f4d5451a09c72a2013e624af21c72c1ad3
golbatnio <golbatinio@outlook.com>
2014-11-06 20:31:08 +0100

    missing files

---
+++ PlatformerTest/PlatformerTest/Base/Sprite.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace PlatformerTest.Base
+{
+    public class Sprite : Component
+    {
+        #region protected
+
+        protected Texture2D texture;
+        /// <summary>
+        /// Because hitbox rectangle starts from position, its represented
+        /// only in vector(where X is width and Y is height)
+        /// </summary>
+        protected Vector2 hitbox;
+        protected Vector2 position;
+        /// <summary>
+        /// Path to texture content
+        /// </summary>
+        protected string textureSource;
+
+        #endregion
+
+        #region properties
+
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        #endregion
+
+        #region constructors
+
+        protected Sprite (string textureSource, Vector2 hitbox)
+        {
+            this.textureSource = textureSource;
+            this.hitbox = hitbox;
+        }
+
+        #endregion
+
+        #region overrides
+
+        public override void LoadContent(ContentManager content)
+        {
+            texture = content.Load<Texture2D>(textureSource);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture,
+                new Rectangle((int)position.X, (int)position.Y, (int)hitbox.X, (int)hitbox.Y),
+                Color.White);
+        }
+
+        #endregion
+
+    }
+}
---
+++ PlatformerTest/PlatformerTest/CameraGame/CameraTestCamera.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using PlatformerTest.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlatformerTest.CameraGame
+{
+    class CameraTestCamera : Base.Camera
+    {
+        public override void SetPosition(Sprite sprite)
+        {
+            position = sprite.Position - new Vector2(ProgramConfig.windowWidth / 2, ProgramConfig.windowHeight / 2);
+        }
+
+        public override Matrix GetTranslationMatrix()
+        {
+            return Matrix.CreateTranslation(-position.X, -position.Y, 0f);
+        }
+    }
+}
